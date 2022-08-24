9afb5d272c2d4ea38e9d8fa3218617c338c39fa7
ke-coder08 <ke.coder08@gmail.com>
2022-08-24 15:35:39 +0800

    init

--- code.js
+++ code.js
@@ -1,18 +1,1 @@
 
-//cursor selector load page
-const cursor = document.querySelector(".pointer");
-
-document.addEventListener("mousemove",(event)=>{
-        cursor.style.left =
-        `${event.clientX}px`;
-
-        cursor.style.top =
-        `${event.clientY}px`;
-
-        cursor.style.display="block";
-})
-
-document.addEventListener("nomouse",()=>{
-    cursor.style.display="none";
-})
-
