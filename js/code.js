b976813e552c1baab5d0c814738e145c2e025237
ke-coder08 <ke.coder08@gmail.com>
2022-09-12 15:18:36 +0800

    init

--- js/code.js
+++ js/code.js
@@ -1,17 +1,30 @@
 
 //SendEmail
 function SendEmail(){
-  Email.send({
+  var name = document.getElementById('name').value;
+  var subject = document.getElementById('subject').value;
+  var cname = document.getElementById('companyname').value;
+  var email = document.getElementById('email').value;
+
+  if (name === "" || subject === "" || cname === "" || email === ""){
+    alert('Please fill all the fields first');
+  }
+
+  else{
+    Email.send({
       Host : "smtp.elasticemail.com",
       Username : 'ke.coder08@gmail.com',
       Password : "E51C4A9C7E4F749A63EEBA8320FC849C4754",
       To : 'ke.coder08@gmail.com',
       From : 'ke.coder08@gmail.com',
-      Subject : document.getElementById('subject').value,
-      Body : "Name: " + document.getElementById('name').value + "<br>" + "Company Name: " + document.getElementById('companyname').value
-          + "<br>" + "Email: " + document.getElementById('email').value + "<br>" +  "Message: " + document.getElementById('message').value
+      Subject : subject,
+      Body : "Name: " + name + "<br>" + "Company Name: " + cname
+          + "<br>" + "Email: " + email
   }).then(
     alert('Successfully Submitted'),
-    document.getElementById('myform').reset()
   );
 }
+  }
+
+ 
+
